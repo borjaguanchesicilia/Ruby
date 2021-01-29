d54ad5fc40ce912e74fef68bf6470073d213d529
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-01-29 13:08:53 +0000

    PF: Func Orden Superior (SRC 1)

--- Programacion-declarativa/Programacion-funional/funcionOrdenSuperior.rb
+++ Programacion-declarativa/Programacion-funional/funcionOrdenSuperior.rb
@@ -18,4 +18,12 @@ objeto_bloque_b = lambda {|x| x = x * 10; puts x}
 
 objeto_bloque_b.call([5])
 objeto_bloque_b.call("h")
-objeto_bloque_b.call(100, 10)
+# Ahora falla, porque se espera un argumento,no dos
+#objeto_bloque_b.call(100, 10)
+
+# Para poder introducir más argumentos debemos:
+
+puts "\n\nTransformando un bloque en un objeto con lambdas con varios argumentos:"
+
+objeto_bloque_c = lambda {|x, y| x = x * 10; y = y * 100; puts x, y}
+objeto_bloque_c.call(100, 10)
