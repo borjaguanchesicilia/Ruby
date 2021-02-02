bd31e394c7b38b5dbc332f995510a55a60ee13f5
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-02-02 11:27:49 +0000

    PC: Threads (SRC 2)

--- Programacion-concurrente/thread.rb
+++ Programacion-concurrente/thread.rb
@@ -5,16 +5,24 @@ require 'thread'
 mutex = Mutex.new
  
 cv = ConditionVariable.new()
+
+panes = 0
  
 t1 = Thread.new() {
  
     mutex.synchronize {
  
-        puts "Ha llegado un cliente y quiere un pan normal... EL cliente debe esperar a que el panadero termine de hornear."
+        puts "Ha llegado un cliente y quiere un pan normal... El cliente debe esperar a que el panadero termine de hornear."
  
         cv.wait(mutex)
+
+        puts "El total de panes en el expositor es #{panes}"
   
         puts "El panadero ya le puede vender el pan al cliente."
+
+        panes -= 1
+
+        puts "El total de panes en el expositor es #{panes}"
     }
 }
  
@@ -27,6 +35,8 @@ t2 = Thread.new() {
         cv.signal
  
         puts "El panadero saca la hornada de pan al expositor."
+
+        panes += 1
     }
 }
  
